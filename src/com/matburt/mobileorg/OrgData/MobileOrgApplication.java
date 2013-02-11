a9a13e0e0700df352db5d6e6b64ecff43bebca4b
Aaron Peromsik <aaron@peromsik.net>
2013-02-10 22:32:21 -0500

    Call SQLiteDatabase.loadLibs().

--- src/com/matburt/mobileorg/OrgData/MobileOrgApplication.java
+++ src/com/matburt/mobileorg/OrgData/MobileOrgApplication.java
@@ -1,5 +1,6 @@
 package com.matburt.mobileorg.OrgData;
 
+import net.sqlcipher.database.SQLiteDatabase;
 import android.app.Application;
 
 import com.matburt.mobileorg.Services.SyncService;
@@ -8,6 +9,7 @@ public class MobileOrgApplication extends Application {
     
     @Override
     public void onCreate() {
+    	SQLiteDatabase.loadLibs(this);
 		SyncService.startAlarm(getApplicationContext());
     }
 }
